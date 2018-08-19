c9343178af059405f73859bebb3b49aae22ed96d
Jense <291359848@qq.com>
2018-08-19 08:39:49 +0800

    remote something in the main.c

--- QG_BRACELET/User/main.c
+++ QG_BRACELET/User/main.c
@@ -6,33 +6,13 @@
 #include <stdio.h>
 #include <string.h>
 
-extern char error_result[20];
-extern char Uart2_Buf[Buf2_Max];
-extern uint32_t First_Int;
-
 
 int main(void)
 {
-	uint8_t i;
+
 	
 	// 来到这里的时候，系统的时钟已经被配置成72M。
 	SysTick_Init(72);
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);  //中断优先级分组 分2组
-	USART2_Init_Config(115200);  //串口2初始化
-	USART1_Init_Config(115200);
-	//等待系统稳定
-	for(i = 0;i < STABLE_TIMES;i++)
-	{
-		delay_ms(50);
-	}
-	
-	while(1)
-	{
-		Send_data_and_error_deal(Server_IP_port ,"12346789798879848464684646874684356484645646845645456468468464684");
-		for(i=0;i<30;i++)
-		{
-			delay_ms(200);
-		}
-	}
 	
 }
