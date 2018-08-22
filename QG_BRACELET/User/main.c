cfebcc8dabc837cd844df3f50dac559d95c08def
Jense <291359848@qq.com>
2018-08-22 09:12:30 +0800

    代码基本整合好

--- QG_BRACELET/User/main.c
+++ QG_BRACELET/User/main.c
@@ -10,44 +10,47 @@
 
 
 extern GPS_Data_Typedef GPS_Data;
-extern double latitude;   //γ��
-extern double longitude;	//����
-extern int32_t sp02[20];              //Ѫ�����Ͷȴ洢����
-extern int32_t heart_rate[20];        //����ֵ�洢����
+extern double latitude;   //纬度
+extern double longitude;	//经度
+extern int32_t sp02[20];              //血氧浓度
+extern int32_t heart_rate[20];        //心率
 
 
 int main(void)
 {
 	uint8_t i;
-	// ���������ʱ��ϵͳ��ʱ���Ѿ������ó�72M��
+	// 配置成72M
 	SysTick_Init(72);
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);  //�ж����ȼ����� ��2��
-	//��ӡ����GPRSʱ��115200�Ĳ����ʣ�����GPSʱ����9600
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);  //中断分2组
+	//调试GPRS时用115200的波特率，调试GPS时用9600的波特率
 	USART1_Init_Config(115200);
-	//��ʼ������2
+	//初始化串口2
 	USART2_Init_Config(115200);
 	
-	UART1_SendString("ϵͳ��.......................\r\n");
-	//��ʱһ�£��ȴ�ϵͳ�ȶ�
+	
+	//等待系统初始化
 	for(i=0;i<20;i++)
 		delay_ms(100);
 	while(1)
 	{
 		char data_to_serve[200];
-		max30102_main();                 //max30102������������Ѫ���������ݣ����ȵ��øú�������ȡֵ��
-									  //����һ�δ����Ҫ9s���������ݣ��ɼ�С����������    
-	 
-	 	get_aver_hr(heart_rate);         //�ú�������һ������ֵ��ƽ��ֵ���˴�û�и�ֵ������ʱ�ɸ�����Ҫȡ��
-	 	get_aver_spO2(sp02);             //�ú�������һ��Ѫ��Ũ�ȵ�ƽ��ֵ���˴�û�и�ֵ������ʱ�ɸ�����Ҫȡ��
-	
+		int heartRate;
+		float sp;
+		max30102_main();                 //max3010初始化
+									 
+	 	heartRate=get_aver_hr(heart_rate);         //获取心率
+	 	sp=get_aver_spO2(sp02); 		//获取血氧浓度
+		
+			//获取GPS数据
 		//parseGpsBuffer();
 		//printf("%lf,%lf\r\n",longitude,latitude);
-		//ת������
-		sprintf(data_to_serve,"lo:%lf,la:%lf",longitude,latitude);
-		//�������ݸ���̨
+		//如果没有报警就发这些数据给后台
+		//转换字符串
+		sprintf(data_to_serve,"lo:%lf,la:%lf,hr:%d,sp:%lf",longitude,latitude,heartRate,sp);
+		//发送给后台
 		Send_data_and_error_deal(Serve_IP_TCP_Port,data_to_serve);
-		for(i=0;i<20;i++)
-			delay_ms(100);
+		//如果报警就发help给后台
+		
 	}
 }
 
